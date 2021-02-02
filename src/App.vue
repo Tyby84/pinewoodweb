0fa95c03be949c87cf28c1d6f8b7e37cd64ca5ff
Tyby <tibofenyvesi84@gmail.com>
2021-02-02 21:39:58 +0100

    mixins

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-
+    <section class="menu">
+      <img class="about" src="images/about.png" alt="Pinewood" sizes="" srcset="">
+    </section>
     <div class="content">
       <transition>
         <keep-alive>
@@ -26,4 +28,5 @@ export default {
 <style>
   @import './assets/css/base.css';
   @import './assets/css/mixin.css';
+  @import './assets/css/menu.css';
 </style>
